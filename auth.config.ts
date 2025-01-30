bbf0de2f6f177ae7311d87383a23a357d3d1772f
ANDERSON DE OLIVEIRA E SILVA JUNIOR <87553205+Anderson-OSJr@users.noreply.github.com>
2025-01-30 18:23:28 -0300

    Ater the end of the day, Google token provider set

--- auth.config.ts
+++ auth.config.ts
@@ -3,5 +3,18 @@ import Credentials from "next-auth/providers/credentials";
 import type { NextAuthConfig } from "next-auth";
 
 export default {
-  providers: [Google({}), Credentials({})],
+  providers: [
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
+    }),
+    Credentials({}),
+  ],
 } satisfies NextAuthConfig;
